94130f4e0f75de870dd9acc3ed25113c6ef1f011
vob0x <github.vob0x@passmail.com>
2026-08-04 06:42:17 +0200

    v64: Boss-Roboter hoeher gerueckt (Fussabstand)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // KOSMOBAGGER PWA Service-Worker.
 // App-Shell NETWORK-FIRST (online aktuell), Medien CACHE-FIRST (offline). Version bei Release erhoehen.
-const CACHE = "kosmobagger-v63";
+const CACHE = "kosmobagger-v64";
 const ASSETS = [
   "./",
   "index.html",
